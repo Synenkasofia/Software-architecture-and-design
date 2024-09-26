a35558083577139fc8e089748468ed70fa958a12
Synenkasofia <Synenkasofia@gmail.com>
2024-09-26 21:30:44 +0200

    Create Program2.cs

---
+++ Program2.cs
@@ -0,0 +1,45 @@
+
+//Lsp
+
+
+using System;
+
+interface IFlyable
+{
+    void Fly();
+}
+
+interface ISwimmable
+{
+    void Swim();
+}
+
+class Bird : IFlyable
+{
+    public void Fly() => Console.WriteLine("Птах летить");
+}
+
+class Fish : ISwimmable
+{
+    public void Swim() => Console.WriteLine("Риба плаває");
+}
+
+class Duck : IFlyable, ISwimmable
+{
+    public void Fly() => Console.WriteLine("Качка летить");
+    public void Swim() => Console.WriteLine("Качка плаває");
+}
+
+class Program
+{
+    static void Main()
+    {
+        new Bird().Fly();
+        new Fish().Swim();
+        new Duck().Fly();
+        new Duck().Swim();
+    }
+}
+
+
+
