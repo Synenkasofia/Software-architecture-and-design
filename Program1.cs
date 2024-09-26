cf78c4ca4bf6b1a54a362bbf8935df49717adcaa
Synenkasofia <Synenkasofia@gmail.com>
2024-09-26 21:29:07 +0200

    Create Program1.cs

---
+++ Program1.cs
@@ -0,0 +1,36 @@
+//OCP
+
+
+using System;
+
+class PaymentProcessor
+{
+    public virtual void ProcessPayment() {}
+}
+
+class CreditCardPayment : PaymentProcessor
+{
+    public override void ProcessPayment()
+    {
+        Console.WriteLine("Кредитна карта");
+    }
+}
+
+class PayPalPayment : PaymentProcessor
+{
+    public override void ProcessPayment()
+    {
+        Console.WriteLine("PayPal");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        new CreditCardPayment().ProcessPayment();
+        new PayPalPayment().ProcessPayment();
+    }
+}
+
+
